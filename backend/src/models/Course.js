6e61381cb2235b10980c7ab3584d655e1d9bd0cc
Nishit4646 <patelnishit1060@gmail.com>
2026-03-17 15:59:30 +0530

    fix: category enum issue

--- backend/src/models/Course.js
+++ backend/src/models/Course.js
@@ -16,7 +16,7 @@ const courseSchema = new mongoose.Schema(
       required: [true, "Category is required"],
       
       enum: {
-        values: ["frontend", "backend", "database", "fullstack", "general","programming",],
+        values: ["frontend", "backend", "database", "fullstack", "general","programming", "design"],
         message: '{VALUE} is not a valid category'
       },
       lowercase: true, 
